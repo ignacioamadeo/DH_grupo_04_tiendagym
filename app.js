c6aa61588e423775c03a562392cc9b90f4d7e8bb
gtiavo <victorg.avila@gmail.com>
2021-09-06 12:32:05 -0300

    termine de crear rutas en la carpeta routes y use el metodo render

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+//apartir de aca comienza el sprint3 con el metodo set para ejs y las rutas
 app.set('view engine', 'ejs');
 let rutasIndex= require('./routes/index.js');
-
+let rutasCarrito=require('./routes/productCart.js');
+let rutasProductDetail=require('./routes/productDetail.js');
+let rutasProductIndex=require('./routes/productindex.js');
+let rutasLogin=require('./routes/login.js');
+let rutasRegister=require('./routes/register.js');
+//===========================================================================
 app.use(express.static(path.join(__dirname,'./public')));
 
 //==========================================================================
@@ -15,25 +21,42 @@ app.use(express.static(path.join(__dirname,'./public')));
 //   });
 
 
-app.get('/productCart', (req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/productCart.html'))
-});
+app.use('/productCart',rutasCarrito);
 
-app.get('/productDetail',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/productDetail.html'))
-})
 
-app.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/login.html'))
-})
+// app.get('/productCart', (req,res)=>{
+//     res.sendFile(path.join(__dirname,'./views/productCart.html'))
+// });
 
-app.get('/productIndex',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/productIndex.html'))
-})
 
-app.get('/register',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./views/register.html'))
-})
+
+app.use('/productDetail', rutasProductDetail);
+
+// app.get('/productDetail',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'./views/productDetail.html'))
+// })
+
+
+app.use('/login', rutasLogin);
+
+// app.get('/login',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'./views/login.html'))
+// })
+
+
+app.use('/productIndex', rutasProductIndex);
+
+// app.get('/productIndex',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'./views/productIndex.html'))
+// })
+
+
+
+app.use('/register', rutasRegister);
+
+// app.get('/register',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'./views/register.html'))
+// })
 
 
 const puerto = process.env.PORT || 3000
