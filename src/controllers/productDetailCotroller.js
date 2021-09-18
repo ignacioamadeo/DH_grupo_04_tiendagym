ccfa39673fe20dc3d5b7aa0a961e80a1121ccd90
gtiavo <victorg.avila@gmail.com>
2021-09-18 12:25:44 -0300

    agregue rutas parametrizadas al productIndex/detalle mas json

--- src/controllers/productDetailCotroller.js
+++ src/controllers/productDetailCotroller.js
@@ -1,7 +1,12 @@
+const db=require('../modules/procuctsIndex.json');
+
 let productDetailController = {
 
 productDetail: (req,res)=>{
-    res.render('products/productDetail'); //al crear carpetas en views y agregarle archivos, la ruta en el controlador debera ser nombrada con el nombre de la carpeta a la que pertenece
+    let product = db.find(item => item.id == req.params.idProductDetail)
+    res.render('products/productDetail', {product:product}); //al crear carpetas en views y agregarle archivos, la ruta en el controlador debera ser nombrada con el nombre de la carpeta a la que pertenece
+
+
 }
 
 
