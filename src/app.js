c72f6db759ab95bb9c6635d9e6355aafc3c7d0de
ignacioamadeo <ignacio.amadeo@gmail.com>
2021-10-25 19:04:13 -0300

    Agrego el require que faltaba para cookie-parser

--- src/app.js
+++ src/app.js
@@ -23,8 +23,6 @@ app.use(methodOverride('_method'));
 //CONFIG PUBLIC - Hago pública la carpeta public:
 app.use(express.static(path.join(__dirname,'./public'))); 
 
-
-
 //CONFIG MIDDLEWARE - :
 const userLoggedMiddleware = require('./middleware/userLoggedMiddleware');
 const recordameMiddleware = require('./middleware/recordameMiddleware');
@@ -32,8 +30,10 @@ app.use(express.json());
 app.use(session({ secret: 'shhhh', resave: false, saveUninitialized: false,}))
 app.use(userLoggedMiddleware);
 app.use(express.urlencoded({extended:false}));
+const cookieParser = require('cookie-parser')
 app.use(cookieParser());
 app.use(recordameMiddleware);
+
 /* --- 
 RUTEO: 
 --- */
