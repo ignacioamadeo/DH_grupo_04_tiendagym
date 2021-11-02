a51611b5df770b96c0a6ad17f37d41a1a63d3730
gtiavo <victorg.avila@gmail.com>
2021-11-02 19:45:48 -0300

    agregue el modelo para la tabla Users

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,29 +1,31 @@
-module.exports = (sequelize, dataTypes)=>{
+module.exports = (sequelize, DataTypes)=>{
     let alias = 'Users';
     let cols = {
         id:{
-
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true 
         },
         firstName:{
-
+            type: DataTypes.STRING  
         },
         lastName:{
-
+            type: DataTypes.STRING  
         },
         email:{
-
+            type: DataTypes.STRING
         },
         username:{
-
+            type: DataTypes.STRING
         },
         password:{
-
+            type: DataTypes.STRING
         },
         image:{
-
+            type: DataTypes.STRING
         },
         userType:{
-            
+            type:DataTypes.BOOLEAN 
         }
 
     };
