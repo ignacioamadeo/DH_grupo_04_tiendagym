f14bad6b2334438b327da6d9a5bf136ab79363d1
gtiavo <victorg.avila@gmail.com>
2021-12-04 16:50:40 -0300

    modo nocturno terminado

--- src/public/js/modoNocturno.js
+++ src/public/js/modoNocturno.js
@@ -13,8 +13,21 @@ window.addEventListener('load', function (){
     const footerUlLi = document.querySelector('.footerLinks ul li');
     const footerI = document.querySelector('.redesSociales a i');
     
+    if(localStorage.getItem('cambiarModo') == 'noche'){
+        apagado.style.display = 'block'
+        apagado.style.color = 'white'
+        encendido.style.display = 'none'
+        body.style.backgroundColor = 'black'
+        naVar.style. backgroundColor = 'black';
+        naVar1.style. backgroundColor = 'black';
+        naVar2.style. backgroundColor = '#022B3A';
+        naVarA.style.color = 'white';
+        footer.style. backgroundColor = '#022B3A';
+        footerUlLi.style.color = 'white';
+        footerI.style.color = 'white';
+    }
     let contador = 1
-   let modoNocturno = btn.addEventListener('click', function (){
+    btn.addEventListener('click', function (e){ 
        if(contador == 1){
         apagado.style.display = 'block'
         apagado.style.color = 'white'
@@ -27,16 +40,16 @@ window.addEventListener('load', function (){
         footer.style. backgroundColor = '#022B3A';
         footerUlLi.style.color = 'white';
         footerI.style.color = 'white';
-
+        localStorage.setItem('cambiarModo', 'noche' )
        
         contador = 0
+
        }else{
         contador = 1
         apagado.style.display = 'none'
         encendido.style.display = 'block'
         encendido.style.color = 'white'
         body.style.backgroundColor = 'white'
-
         naVar.style. backgroundColor = '#022B3A';
         naVar1.style. backgroundColor = '#022B3A';
         naVar2.style. backgroundColor = '#3b5964';
@@ -44,8 +57,9 @@ window.addEventListener('load', function (){
         footer.style. backgroundColor = '#e1e5f2';
         footerUlLi.style.color = '#022B3A';
         footerI.style.color = '#022B3A';
+        localStorage.setItem('cambiarModo', 'dia' )
        }
     })
+   
     // localStorage.clear()
-    // localStorage.setItem('cambiarModo', modoNocturno )
 })
