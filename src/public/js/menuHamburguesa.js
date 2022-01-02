8c2ba5b0958f9f0856a6473adae9b836ff180c91
ignacioamadeo <ignacio.amadeo@gmail.com>
2022-01-02 16:08:33 -0300

    Casi funcional animación botón home versión mobile para usuarios logueados

--- src/public/js/menuHamburguesa.js
+++ src/public/js/menuHamburguesa.js
@@ -1,11 +1,16 @@
 window.addEventListener('load', function(){
     const menuHamb = document.querySelector('.botonMenuMobile');
     const menu = document.querySelector('.mobMainHeader');
-    const logoProfile = document.querySelector('.logoHeaderProfile');
+    const logoProfile = document.getElementById('logoHeaderProfile');
+    const usuarioLogueado = document.querySelector('.barraProfile');
+    const textoLogoProfile = document.querySelector('.userLogHelp');
 
     menuHamb.onclick = function(e){
         menuHamb.classList.toggle('botonMenuMobileRot');
         menu.classList.toggle('mobMainHeaderAmpliado');
+        logoProfile.classList.toggle('logoHeaderProfileShow');
+        textoLogoProfile.classList.toggle('userLogHelpNo');
+        usuarioLogueado.classList.toggle('barraProfileOff');
     }
    
 })
