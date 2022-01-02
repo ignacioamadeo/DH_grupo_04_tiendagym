61b3a7e9f1e351b4126edd3d2e1bf6d31300c89f
ignacioamadeo <ignacio.amadeo@gmail.com>
2022-01-02 00:05:28 -0300

    Avances en header versión mobile

---
+++ src/public/js/menuHamburguesa.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(){
+    const menuHamb = document.querySelector('.botonMenuMobile');
+    const menu = document.querySelector('.mobMainHeader');
+    const logoProfile = document.querySelector('.logoHeaderProfile');
+
+
+   
+})
