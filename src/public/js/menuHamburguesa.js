0ab081e6f0df7e16b368686baba498df1495fb85
ignacioamadeo <ignacio.amadeo@gmail.com>
2022-01-02 23:29:50 -0300

    Terminado home versión mobile

--- src/public/js/menuHamburguesa.js
+++ src/public/js/menuHamburguesa.js
@@ -1,15 +1,15 @@
 window.addEventListener('load', function(){
     const menuHamb = document.querySelector('.botonMenuMobile');
     const menu = document.querySelector('.mobMainHeader');
-    const logoProfile = document.getElementById('logoHeaderProfile');
-    const usuarioLogueado = document.querySelector('.barraProfile');
-    const textoLogoProfile = document.querySelector('.userLogHelp');
+    const logoProfile = document.getElementById('logoHeaderProfile'); //Logo 
+    const textoLogoProfile = document.querySelector('.userLogHelp'); //Texto "usuario no ingresado"
+    const usuarioLogueado = document.getElementById('barraProfile'); //Perfil usuario
 
     menuHamb.onclick = function(e){
         menuHamb.classList.toggle('botonMenuMobileRot');
         menu.classList.toggle('mobMainHeaderAmpliado');
-        logoProfile.classList.toggle('logoHeaderProfileShow');
-        textoLogoProfile.classList.toggle('userLogHelpNo');
+        logoProfile?.classList.toggle('logoHeaderProfileShow');
+        textoLogoProfile?.classList.toggle('userLogHelpNo');
         usuarioLogueado.classList.toggle('barraProfileOff');
     }
    
